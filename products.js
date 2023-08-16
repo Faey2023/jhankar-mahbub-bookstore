aaa7b3248e53b5e71c28ece545f243d1bd415657
Fahima <sfahima1388@gmail.com>
2023-08-16 12:38:17 +0600

    product div added with js

---
+++ products.js
@@ -0,0 +1,146 @@
+const products = [
+  // 1st book
+  {
+    id: 1,
+    name: "চেষ্টার জিমনেসিয়াম ফিউচারের ক্যালসিয়াম",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1456904905i/29391170.jpg",
+    originalPrice: 420,
+    discountPrice: 361,
+    inStock: true,
+    rating: 3.82,
+    reviews: [
+      {
+        id: 1,
+        name: "Jhankar Mahbub",
+        review: "আমার এই বই খুবই ভালো। কিনে ফেলো। ",
+      },
+      { id: 2, name: "Faheema", review: "Speachless!!! " },
+      {
+        id: 3,
+        name: "Md. Hasib Imam",
+        review: "একদম সেরা বই!!! ",
+      },
+    ],
+  },
+  //   2nd book
+  {
+    id: 2,
+    name: "রিচার্জ your ডাউন ব্যাটারি",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1516650964i/38124405.jpg",
+    originalPrice: 420,
+    discountPrice: 361,
+    inStock: true,
+    rating: 3.92,
+    reviews: [
+      {
+        id: 1,
+        name: "Jhankar Mahbub",
+        review: "আমার এই বই খুবই ভালো। কিনে ফেলো। ",
+      },
+      { id: 2, name: "Faheema", review: "Speachless!!! " },
+      {
+        id: 3,
+        name: "Md. Hasib Imam",
+        review: "একদম সেরা বই!!! ",
+      },
+    ],
+  },
+  //   3rd book
+  {
+    id: 3,
+    name: "প্রোগ্রামিংয়ের বলদ to বস",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1486978070i/34259074.jpg",
+    originalPrice: 420,
+    discountPrice: 361,
+    inStock: true,
+    rating: 4.33,
+    reviews: [
+      {
+        id: 1,
+        name: "Jhankar Mahbub",
+        review: "আমার এই বই খুবই ভালো। কিনে ফেলো। ",
+      },
+      { id: 2, name: "Faheema", review: "Speachless!!! " },
+      {
+        id: 3,
+        name: "Md. Hasib Imam",
+        review: "একদম সেরা বই!!! ",
+      },
+    ],
+  },
+  //   4th book
+  {
+    id: 4,
+    name: "প্যারাময় লাইফের প্যারাসিটামল",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1550474511i/44047186.jpg",
+    originalPrice: 420,
+    discountPrice: 361,
+    inStock: true,
+    rating: 4,
+    reviews: [
+      {
+        id: 1,
+        name: "Jhankar Mahbub",
+        review: "আমার এই বই খুবই ভালো। কিনে ফেলো। ",
+      },
+      { id: 2, name: "Faheema", review: "Speachless!!! " },
+      {
+        id: 3,
+        name: "Md. Hasib Imam",
+        review: "একদম সেরা বই!!! ",
+      },
+    ],
+  },
+  //   5th book
+  {
+    id: 5,
+    name: "প্রোগ্রামিংয়ের চৌদ্দগোষ্ঠী",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1525996046i/40098190.jpg",
+    originalPrice: 420,
+    discountPrice: 361,
+    inStock: true,
+    rating: 4.55,
+    reviews: [
+      {
+        id: 1,
+        name: "Jhankar Mahbub",
+        review: "আমার এই বই খুবই ভালো। কিনে ফেলো। ",
+      },
+      { id: 2, name: "Faheema", review: "Speachless!!! " },
+      {
+        id: 3,
+        name: "Md. Hasib Imam",
+        review: "একদম সেরা বই!!! ",
+      },
+    ],
+  },
+  //   6th book
+  {
+    id: 6,
+    name: "হাবলুদের জন্য প্রোগ্রামিং",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1456904905i/29391170.jpg",
+    originalPrice: 420,
+    discountPrice: 361,
+    inStock: true,
+    rating: 4.06,
+    reviews: [
+      {
+        id: 1,
+        name: "Jhankar Mahbub",
+        review: "আমার এই বই খুবই ভালো। কিনে ফেলো। ",
+      },
+      { id: 2, name: "Faheema", review: "Speachless!!! " },
+      {
+        id: 3,
+        name: "Md. Hasib Imam",
+        review: "একদম সেরা বই!!! ",
+      },
+    ],
+  },
+];
